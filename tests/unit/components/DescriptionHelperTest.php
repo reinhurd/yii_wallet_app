4ec376f98b0589ae8aa883d35f5e993fe6e1168a
reinhurd <roarus@yandex.ru>
2021-09-20 23:38:59 +0400

    Add test for description helper

---
+++ tests/unit/components/DescriptionHelperTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace tests\unit\components;
+
+use app\components\helpers\DescriptionHelper;
+use Codeception\Test\Unit;
+
+class DescriptionHelperTest extends Unit
+{
+    private $helper;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->helper = \Yii::createObject(DescriptionHelper::class);
+    }
+
+    public function testGetDescriptionFromArray(): void
+    {
+        $array = [1=>'one', 2=>'two'];
+        $delimeter = ' => ';
+        $expected = '
+            ' . 1 . $delimeter . 'one' . '
+            ' . 2 . $delimeter . 'two';
+        $result = $this->helper->getDescriptionFromArray($array);
+
+        $this->assertEquals($expected, $result);
+    }
+}
